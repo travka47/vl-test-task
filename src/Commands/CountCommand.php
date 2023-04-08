7e308b96ed5af8ec83ab03e913bcb199a4f1e09d
travka47 <travka.eal@students.dvfu.ru>
2023-04-09 00:07:04 +1000

    base counter

--- src/Commands/CountCommand.php
+++ src/Commands/CountCommand.php
@@ -3,21 +3,39 @@ namespace Console\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 
 class CountCommand extends Command
 {
-    public function configure()
+    public function configure() : void
     {
         $this
             ->setName('count')
             ->setDescription('Count values in .count files');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('counter');
+        $output->writeln('sum: ' . $this->count());
         return self::SUCCESS;
     }
 
+    private function count() : float
+    {
+        $rootPath = __DIR__ . '/../..';
+
+        $finder = new Finder();
+        $finder->files()->in($rootPath)->exclude('vendor')->name('count');
+
+        $result = 0;
+
+        foreach ($finder as $file) {
+            $content = $file->getContents();
+            $result += is_numeric($content) ? $content : 0;
+            echo $file->getRelativePathname() . ' ' . $content . '  ';
+        }
+
+        return $result;
+    }
 }
